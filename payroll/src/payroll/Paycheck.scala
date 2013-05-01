ab86b7cb6baed3fed08d5aa39d64c4e33cde26d5
Julien De Coster <julien.decoster90@gmail.com>
2013-05-01 17:17:30 +0200

    Delete useless files

--- payroll/src/payroll/Paycheck.scala
+++
@@ -1,8 +0,0 @@
-package payroll
-
-case class Paycheck(gross: Money, net: Money, deduction: Money) {
-
-  def plusGross(m: Money) 	= Paycheck(gross + m, 	net + m, deduction)
-  def plusDeduction(m: Money) = Paycheck(gross, 	net - m, deduction + m)
-  
-}
