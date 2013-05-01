6fe442bd12457af38b7937f67ddcd97890f67e92
Xavier Crochet <xacrochet@gmail.com>
2013-05-01 17:20:09 +0200

    Removed useless stuff

--- src/Facebook.scala
+++ src/Facebook.scala
@@ -1,21 +1,8 @@
 class Facebook(val m:SimModel, val population: List[People])
 {
   
-  def idle() {
-    /*for (p <- population; if p.facebook == false)
-      invite(p)
-  
-    m.wait(5.0) {
-      idle()
-    }*/
-
-  }
-
   def invite(p: People) {
     println("Facebook has invited " + p.name)
-    p.readFacebookInvitation()
-    m.wait(5.0) {
-      idle()
-    }
+    p.facebookInvitations :+ new FacebookInvitation(p)
   }
 }
--- src/People.scala
+++ src/People.scala
@@ -3,22 +3,6 @@ class People(val name: String, val m: SimModel, var facebook: Boolean) {
   var facebookProfile : FacebookProfile = new FacebookProfile(this)
   var facebookInvitations = List[FacebookInvitation]()  
   
-  def idle() {
-    println(name +" is idle.")
-    var r = new scala.util.Random()
-    m.wait(10.0) {
-      if(facebook) {
-        if (r.nextInt() % 10 == 0)
-          leaveFacebook()
-      }
-      else {
-        if (r.nextInt() % 10 == 0)
-          joinFacebook()
-      }
-      idle()
-    }
-  }
-
   def joinFacebook() {
     println(name + " has join Facebook")
     facebook = true
@@ -27,15 +11,5 @@ class People(val name: String, val m: SimModel, var facebook: Boolean) {
   def leaveFacebook() {
     println(name + " has left Facebook")
     facebook = false
-  }
-  
-  def readFacebookInvitation() {
-    var r = new scala.util.Random()
-    if (r.nextInt() % 2 == 0) {
-      println(name + " has accepted Facebook's invitation! ")
-      joinFacebook()
-    }
-    else
-      println(name + " has refused Facebook's invitation! ")
   }
 }
