50427b844cad924299ec7d957090760bb8cb3f59
Julien De Coster <julien.decoster90@gmail.com>
2013-05-08 22:16:43 +0200

    Clean main

--- src/sim/Main.scala
+++ src/sim/Main.scala
@@ -43,50 +43,16 @@ object App {
 
 
     /* SCENARIOS CONFIGURATION */
-    Scenario1 ( Teenagers join  ( Twitter when_receive   2.invitations ))
+    Scenario1 ( Teenagers join  ( Twitter  when_receive   2.invitations ))
     Scenario1 ( Adults    join  ( Facebook when_receive   2.invitations ))
-    //Scenario1 ( Students  join  ( Twitter  when_receive   2.invitations ))
     Scenario1 ( Adults    join  ( Youtube  when_receive   2.invitations ))
-    //Scenario1 ( Students  join  ( Youtube  when_receive   2.invitations ))
-    //Scenario1 ( Teenagers leave ( Youtube  when_more_than 1.subscribers ))
-    //Scenario1 ( Teenagers leave ( Facebook when_receive   1.messages    ))
+    Scenario1 ( Teenagers leave ( Youtube  when_more_than 4.subscribers ))
     Scenario1 ( Teenagers leave ( Twitter  when_more_than 3.followers   ))
 
-    //Scenario1 ( Adults    leave ( Youtube  when_more_than 1.subscribers ))
-    //Scenario1 ( Adults    leave ( Twitter  when_more_than 1.followers   ))
-    //Scenario1 ( Adults    leave ( Facebook when_receive   1.messages    ))
-    //Scenario1 ( Students  leave ( Youtube  when_more_than 1.subscribers ))
-    //Scenario1 ( Students  leave ( Twitter  when_more_than 1.followers   ))
-    //Scenario1 ( Adults leave ( Facebook when_more_than 5.friends ))
-
     Scenario2 ( Teenagers leave  ( Twitter when 5.adults are_on Youtube ))
-    // Scenario3 ( Students switch_from ( Facebook to ( Twitter when 5.friends_switch )))
-
-    /* TESTING */
-    println("############")
-    println("DSL Population : " + Population.nbPop)
-    println("DSL Students : " + Population.students)
-    println("DSL Adults : " + Population.adults)
-    println("DSL Teenagers : " + Population.teenagers)
-    println("DSL Facebook : " + Facebook.nbSub)
-    println("DSL Twitter : " + Twitter.nbSub)
-    println("DSL Youtube : " + Youtube.nbSub)
-    println("############")
     
     /* RUN THE SIMULATION ! */
     Run the_simulation_for 50.weeks
-    
-    println("############")
-    println("DSL Population : " + Population.nbPop)
-    println("DSL Students : " + Population.students)
-    println("DSL Adults : " + Population.adults)
-    println("DSL Teenagers : " + Population.teenagers)
-    println("DSL Facebook : " + Facebook.nbSub)
-    println("DSL Twitter : " + Twitter.nbSub)
-    println("DSL Youtube : " + Youtube.nbSub)
-    println("############") 
-
-    Stats.show
     
     Show chart
   }
